0a9251c82e0eb5e8856681c890da342f7adb55c7
Abbas Salah <abbas.log01@gmail.com>
2022-04-06 00:35:32 +0300

    cart context

--- src/contexts/CartContext.js
+++ src/contexts/CartContext.js
@@ -1,5 +1,6 @@
 import { createContext, useEffect, useState } from "react";
 import axios from "axios";
+import { useSnackbar } from 'react-simple-snackbar'
 import {BASE_URL, TOKEN_STR} from "../utils/Constants";
 const initValue = {
     carts:[],
@@ -14,6 +15,22 @@ const initValue = {
 const CartContext = createContext(initValue)
 
 export const CartProvider = ({children})=>{
+    const options = {
+        position: 'bottom-right',
+        style: {
+            backgroundColor: '#39818d',
+            border: '1px',
+            color: 'white',
+            fontFamily: 'Poppins',
+            fontSize: '17px',
+            textAlign: 'center',
+        },
+        closeStyle: {
+            color: 'red',
+            fontSize: '15px',
+        },
+    }
+    const [openSnackbar, closeSnackbar] = useSnackbar(options)
     const [carts, setCarts] = useState([])
     const [total, setTotal] = useState(0)
     const [loading, setLoading] = useState(true)
@@ -43,13 +60,16 @@ export const CartProvider = ({children})=>{
             .then((res)=>{
                 getCart()
             })
+        openSnackbar('Added To Cart Successfully')
     }
     const removeCart = ((id)=>{
         axios.delete(`${BASE_URL}/cart/item/delete/${id}`, { headers: {"Authorization":
-                    `${TOKEN_STR.token.token_type} ${TOKEN_STR.token.access_token}`} })
-        const newCart = carts.filter((item) => item.id !== id);
-        setCarts(newCart);
+                    `${TOKEN_STR.token.token_type} ${TOKEN_STR.token.access_token}`} }).then(()=>{
         setTotal(carts.reduce((total, item)=>total+(item.product.lowest*item.item_qty),0))
+
+        })
+        let newCart = carts.filter((item) => item.id !== id);
+        setCarts(newCart);
     })
     const addQty = ((id, qt)=>{
         axios.post(`${BASE_URL}/cart/item/change-qty/${id}?new_qty=${qt}`, {params: {id: id, new_qty: qt}},
